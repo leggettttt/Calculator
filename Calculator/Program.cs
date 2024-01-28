6fe4a25769f9ae2db01fe8589c98d534dc0a5ae7
leggettttt <benleggo10@gmail.com>
2024-01-28 20:16:16 +0000

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double num1;
+        double num2;
+        double result;
+        do
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("------------------");
+
+            Console.Write("Enter number 1 : ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter number 2 : ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an option : ");
+            Console.WriteLine("\t+ : Add");
+            Console.WriteLine("\t- : Subtract");
+            Console.WriteLine("\t* : Multiply");
+            Console.WriteLine("\t/ : Divide");
+
+            switch (Console.ReadLine())
+            {
+
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                    break;
+                default:
+                    Console.WriteLine("That was not a valid option");
+                    break;
+            }
+            Console.WriteLine("Play again? (Y/N) : ");
+        } while (Console.ReadLine().ToUpper() == "Y");
+
+        Console.WriteLine("Thanks for playing!");
+        Console.ReadKey();
+    }
+}
